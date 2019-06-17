30337f9b8244a4a39286b76e74e32065f7e9224a
Kota Nishinaka <6489627+kotadd@users.noreply.github.com>
2019-06-18 08:20:27 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Replace with your mongoLab URI
 const MONGO_URI =
-  'mongodb+srv://Kota:$Kota3712@lyricaldb-vl0vi.mongodb.net/test?retryWrites=true&w=majority';
+  'mongodb+srv://Kota:Kota@lyricaldb-vl0vi.mongodb.net/test?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
